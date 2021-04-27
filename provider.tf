d469c864c8c443a144e339f2e2d63ec91b819788
Ranjit Swain <ranjitkuswain@gmail.com>
2021-04-27 22:54:28 +0530

    Quote typo fixed

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-provider 'aws' {
+provider "aws" {
 	access_key = "${var.AWS_ACCESS_KEY}"
 	secret_key = "${var.AWS_SECRET_KEY}"
 	region = "${var.AWS_REGION}"
--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
-variable 'AWS_ACCESS_KEY'{}
-variable 'AWS_SECRET_KEY'{}
-variable 'AWS_REGION'{
-	default = 'ap-south-1'
+variable "AWS_ACCESS_KEY'{}
+variable "AWS_SECRET_KEY'{}
+variable "AWS_REGION'{
+	default = "ap-south-1'
 }
